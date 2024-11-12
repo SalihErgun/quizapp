90a5602479ad36c19d6badd8f3670162fd4b0226
Saban Can <sabancan091011@gmail.com>
2024-11-12 22:53:39 +0100

    examclass added

---
+++ examClass.py
@@ -0,0 +1,11 @@
+import time
+#from Question import Question
+#from Timer import Timer
+#from Result import Result
+
+class Exam:
+    def __init__(self, user, questions):
+        pass
+
+def start_exam(self):
+    pass
