6e9e0a612bcf926901b20a79910f70aca5428d29
Saban Can <sabancan091011@gmail.com>
2024-11-13 12:12:55 +0100

    functions added

--- examClass.py
+++ examClass.py
@@ -9,3 +9,25 @@ class Exam:
 
 def start_exam(self):
     pass
+
+def display_question(self, question):
+    pass
+
+
+def get_user_input(self, question):
+    pass
+
+def validate_input(self, user_input, max_options):
+    pass
+
+def time_out(self):
+    pass
+
+def end_exam(self):
+    pass
+
+def calculate_results(self):
+    pass
+
+def show_results(self):
+    pass
